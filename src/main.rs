480f7ffda9e7668dcd346a2d0f13a8b34886973b
Zoltán Balázs <git@zoltanbalazs.com>
2022-06-10 13:44:43 +0200

    Changed installation

--- src/main.rs
+++ src/main.rs
@@ -20,31 +20,38 @@ fn get_bool_answer_to_question<'a>(question: &'a str) -> bool {
         .unwrap_or(false)
 }
 
-fn run_command(command: &str, args: Vec<&str>) {
+fn run_command(string: &str) {
+    let command = string.split_whitespace().next().unwrap();
+    let args = string.split_whitespace().skip(1).collect::<Vec<_>>();
     Command::new(command).args(args).status().expect(format!("failed to run command {}", command).as_str());
 }
 
+fn run_enable_services(services: Vec<&str>) {
+    for service in services {
+        match service {
+            "bluez" => run_command("sudo systemctl enable --now bluetooth.service"),
+            "cups" => run_command("sudo systemctl enable --now cups.service"),
+            "bluez_utils" | "hplip" | "samsung-unified-driver" | "samsung-unified-driver-printer" => (),
+            _ => run_command(format!("sudo systemctl enable --now {}", service).as_str())
+        }
+        
+    }
+}
+
 fn main() -> InquireResult<()> {
-    run_command("sudo", vec!["pacman", "-S", "rustup"]);
-    run_command("rustup", vec!["install", "stable"]);
-    run_command("rustup", vec!["default", "stable"]);
+    run_command("sudo pacman -S rustup --noconfirm");
+    run_command("rustup install stable");
+    run_command("rustup default stable");
 
-    run_command("git", vec!["clone", "https://aur.archlinux.org/paru-bin"]);
+    run_command("git clone https://aur.archlinux.org/paru-bin");
     std::env::set_current_dir("paru-bin").expect("failed to execute process");
-    run_command("makepkg", vec!["-si"]);
+    run_command("makepkg -si");
     std::env::set_current_dir("..").expect("failed to execute process");
-    run_command("rm", vec!["-rf", "paru-bin"]);
-
-    run_command("sudo", vec!["pacman", "-S", "base-devel"]);
-    run_command("git", vec!["clone", "https://aur.archlinux.org/yay.git"]);
-    std::env::set_current_dir("yay").expect("failed to execute process");
-    run_command("makepkg", vec!["-si"]);
-    std::env::set_current_dir("..").expect("failed to execute process");
-    run_command("rm", vec!["-rf", "yay"]);
+    run_command("rm -rf paru-bin");
 
     // sudo::escalate_if_needed().unwrap();
 
-    let _software_packages = vec![
+    let software_packages = vec![
         "blender",
         "celluloid",
         "ferdium",
@@ -70,7 +77,7 @@ fn main() -> InquireResult<()> {
         "visual-studio-code-insiders-bin",
     ];
 
-    let _service_packages = vec![
+    let service_packages = vec![
         "auto-cpufreq",
         "bluez",
         "bluez-utils",
@@ -80,7 +87,7 @@ fn main() -> InquireResult<()> {
         "system76-power",
     ];
 
-    let _font_packages = vec![
+    let font_packages = vec![
         "nerd-fonts-cascadia-code",
         "nerd-fonts-jetbrains-mono",
         "noto-fonts-cjk",
@@ -88,13 +95,13 @@ fn main() -> InquireResult<()> {
         "noto-fonts-extra",
     ];
 
-    let _programming_language_packages = vec![
+    let programming_language_packages = vec![
         "dotnet-sdk-bin",
         "go",
         "rustup",
     ];
 
-    let _utilities_packages = vec![
+    let utilities_packages = vec![
         "bat",
         "exa",
         "fd",
@@ -113,28 +120,62 @@ fn main() -> InquireResult<()> {
 
     println!("{}", Style::new().bold().paint("Zolee's Post x86_64 Archinstall Setup Program"));
 
-    let _software_list = get_list_from_packages("Which software packages do you want to install?", _software_packages);
-    let _service_list = get_list_from_packages("Which services do you want to install?", _service_packages);
-    let _font_list = get_list_from_packages("Which fonts do you want to install?", _font_packages);
-    let _programming_language_list = get_list_from_packages("Which programming languages do you want to install?", _programming_language_packages);
-    let _utilities_list = get_list_from_packages("Which utilities do you want to install?", _utilities_packages);
+    let software_list = get_list_from_packages("Which software packages do you want to install?", software_packages);
+    let service_list = get_list_from_packages("Which services do you want to install?", service_packages);
+    let font_list = get_list_from_packages("Which fonts do you want to install?", font_packages);
+    let programming_language_list = get_list_from_packages("Which programming languages do you want to install?", programming_language_packages);
+    let utilities_list = get_list_from_packages("Which utilities do you want to install?", utilities_packages);
 
-    let _install_bedrock = get_bool_answer_to_question("Install Bedrock Linux?");
-    let _install_theme = get_bool_answer_to_question("Install KDE theme?");
-    let _enable_serivces = get_bool_answer_to_question("Enable installed services?");
+    let install_paru = get_bool_answer_to_question("Install Paru?");
+    let install_bedrock = get_bool_answer_to_question("Install Bedrock Linux?");
+    let install_theme = get_bool_answer_to_question("Install KDE theme?");
+    let install_omf = get_bool_answer_to_question("Install Oh My Fish?");
+    let change_shell = get_bool_answer_to_question("Change Shell to Fish?");
+    let enable_services = get_bool_answer_to_question("Enable installed services?");
 
-    let mut _install_software: Vec<&str> = vec![];
-    _software_list.iter().for_each(|s| _install_software.push(s));
-    _service_list.iter().for_each(|s| _install_software.push(s));
-    _font_list.iter().for_each(|s| _install_software.push(s));
-    _programming_language_list.iter().for_each(|s| _install_software.push(s));
-    _utilities_list.iter().for_each(|s| _install_software.push(s));
+    let mut install_software: Vec<&str> = vec![];
+    software_list.iter().for_each(|s| install_software.push(s));
+    service_list.iter().for_each(|s| install_software.push(s));
+    font_list.iter().for_each(|s| install_software.push(s));
+    programming_language_list.iter().for_each(|s| install_software.push(s));
+    utilities_list.iter().for_each(|s| install_software.push(s));
 
-    let mut s_arg = vec!["-S"];
-    s_arg.append(&mut _install_software);
-    run_command("yay", s_arg);
+    
+    for package in &install_software {
+        run_command(format!("paru -S --noconfirm {}", package).as_str());
+    }
 
-    run_command("paru", vec!["-Rns", "yay"]);
+    if !install_paru {
+        run_command("sudo pacman -Rns paru");
+    }
+
+    if !install_software.contains(&"rustup") {
+        run_command("rustup self uninstall");
+    }
+
+    if enable_services {
+        run_enable_services(service_list);
+    }
+
+    if install_omf {
+        run_command("curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install > install");
+        run_command("fish install");
+        run_command("rm install");
+    }
+
+    if change_shell {
+        run_command("chsh -s /usr/bin/fish");
+    }
+
+    if install_theme {
+
+    }
+
+    if install_bedrock {
+        run_command("curl https://github.com/bedrocklinux/bedrocklinux-userland/releases/download/0.7.27/bedrock-linux-0.7.27-x86_64.sh > bedrock-linux-0.7.27-x86_64.sh");
+        run_command("sh ./bedrock-linux-0.7.27-x86_64.sh --hijack");
+        run_command("rm bedrock-linux-0.7.27-x86_64.sh");
+    }
 
     Ok(())
 }
